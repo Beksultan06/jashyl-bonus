254d3d41c643664d2e8c7bc59878729da78ab1fa
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-11-13 17:16:38 +0600

    commit

--- core/asgi.py
+++ core/asgi.py
@@ -1,7 +1,10 @@
+import os
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import apps.notification.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
